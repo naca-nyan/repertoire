98e0e6b6513a7f224e2bb2688790f2dfe917c774
naca-nyan <naca.nyan@gmail.com>
2022-06-14 14:51:44 +0900

    Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { ChangeEventHandler, MouseEventHandler, useState } from "react";
 import "./App.css";
 import {
   AppBar,
@@ -19,34 +19,41 @@ import SongList from "./SongList";
 import { Search } from "@mui/icons-material";
 import { useDebounce } from "react-use";
 
+const SongListSubHeader: React.FC<{
+  collapsed: boolean;
+  onChangeSearchWord: ChangeEventHandler<HTMLInputElement>;
+  onClickToggleButton: MouseEventHandler;
+}> = ({ collapsed, onChangeSearchWord, onClickToggleButton }) => {
+  return (
+    <ListSubheader component="div">
+      <Toolbar>
+        <Box sx={{ display: "flex", alignItems: "flex-end", flexGrow: 1 }}>
+          <Search sx={{ my: 0.5, mr: 1 }} />
+          <Input placeholder="Search..." onChange={onChangeSearchWord} />
+        </Box>
+        <Button size="small" onClick={onClickToggleButton}>
+          {collapsed ? "Open" : "Close"} All
+        </Button>
+      </Toolbar>
+      <Divider />
+    </ListSubheader>
+  );
+};
+
 function SongListAll() {
   const [searchWord, setSearchWord] = useState("");
   const [debouncedSearchWord, setDebouncedSearchWord] = useState("");
   useDebounce(() => setDebouncedSearchWord(searchWord), 50, [searchWord]);
 
-  const [collapsed, setCollapseAll] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+
+  const subheader = SongListSubHeader({
+    collapsed,
+    onChangeSearchWord: (e) => setSearchWord(e.currentTarget.value),
+    onClickToggleButton: () => setCollapsed(!collapsed),
+  });
   return (
-    <List
-      component="nav"
-      dense
-      subheader={
-        <ListSubheader component="div">
-          <Toolbar>
-            <Box sx={{ display: "flex", alignItems: "flex-end", flexGrow: 1 }}>
-              <Search sx={{ my: 0.5, mr: 1 }} />
-              <Input
-                placeholder="Search..."
-                onChange={(e) => setSearchWord(e.target.value)}
-              />
-            </Box>
-            <Button size="small" onClick={() => setCollapseAll(!collapsed)}>
-              {collapsed ? "Open" : "Close"} All
-            </Button>
-          </Toolbar>
-          <Divider />
-        </ListSubheader>
-      }
-    >
+    <List component="nav" dense subheader={subheader}>
       <SongList
         data={data}
         filter={debouncedSearchWord}
