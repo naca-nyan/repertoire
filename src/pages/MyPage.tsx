8a5f7de66d7257418882077eafd3d7087486579c
naca-nyan <naca.nyan@gmail.com>
2022-06-21 22:30:52 +0900

    Add save button

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, List, Typography } from "@mui/material";
+import { Box, Button, List, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { User } from "firebase/auth";
 import NotFoundPage from "./NotFoundPage";
@@ -7,29 +7,27 @@ import LoadingPage from "./LoadingPage";
 import { Song } from "../song";
 import SongList from "../SongList";
 import SongSubmitForm from "../SongSubmitForm";
-import { ref, set } from "firebase/database";
+import { onValue, ref, set } from "firebase/database";
 import { database } from "../firebase";
 
-async function getMySongs(): Promise<Song[]> {
-  const { default: data } = await import("../data.json");
-  return data;
+const songPath = (userId: string) => `/users/${userId}/songs/`;
+
+function getSongs(userId: string): Promise<Song[]> {
+  return new Promise((resolve, reject) => {
+    onValue(
+      ref(database, songPath(userId)),
+      (snapshot) => {
+        const songs = snapshot.val() || [];
+        resolve(songs);
+      },
+      (error) => reject(error),
+      { onlyOnce: true }
+    );
+  });
 }
 
-async function writeSongs(userId: string, songs: Song[]) {
-  set(ref(database, "/songs/" + userId), songs);
-}
-
-function writeTestSongs(user: User) {
-  // @ts-ignore
-  const userId = user.reloadUserInfo.screenName;
-  const songs = [
-    {
-      title: "test title",
-      artist: "test artist",
-      url: "https://test.url",
-    },
-  ];
-  writeSongs(userId, songs);
+async function setSongs(userId: string, songs: Song[]) {
+  await set(ref(database, songPath(userId)), songs);
 }
 
 interface Props {
@@ -45,16 +43,37 @@ const MyPage: React.FC<Props> = ({ user }) => {
   if (user === null) {
     return <NotFoundPage />;
   }
+
+  // @ts-ignore
+  const userId: string | undefined = user.reloadUserInfo?.screenName;
+  if (!userId) {
+    return <NotFoundPage />;
+  }
+
   if (data === undefined) {
-    getMySongs().then((data) => setData(data));
+    getSongs(userId).then((data) => setData(data));
     return <LoadingPage />;
   }
+
+  function handleSave() {
+    const songs = data;
+    if (!userId || songs === undefined) {
+      console.error("handleSave: cannot save", userId, songs);
+      return;
+    }
+    setSongs(userId, songs);
+  }
+
   return (
     <Container maxWidth="sm" sx={{ mt: 3 }}>
-      <Button onClick={() => writeTestSongs(user)} variant="contained">
-        Send sample data
-      </Button>
-      <Typography variant="h5">Mypage</Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h5">
+          {user.displayName}の知ってる曲リスト
+        </Typography>
+        <Button onClick={handleSave} variant="contained">
+          Save
+        </Button>
+      </Box>
       <List component="nav" dense>
         <SongList data={data} filter="" collapsed={false} />
       </List>
